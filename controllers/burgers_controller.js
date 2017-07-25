98619749d8be66cfa3eff4a923ff54cf1b14d6fe
bcmiller713 <bcmiller713@gmail.com>
2017-07-25 11:06:29 -0700

    completed orm.js, burger.js, and burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,23 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
+router.get("/", function(req, res) {
+	burger.selectAll(function(data) {
+		res.render("index", {burgers: data});
+	});
+});
+
+router.post("/", function(req, res) {
+	burger.insertOne("burger_name", req.body.name, function() {
+		res.redirect("/");
+	});
+});
+
+router.put("/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+	burger.updateOne({id: req.params.id}, condition, function() {
+		res.redirect("/");
+	});
+});
+
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-	selectAll: function() {
-
+	selectAll: function(callback) {
+		orm.selectAll("burgers", function(res) {
+			callback(res);
+		});
 	},
-	insertOne: function() {
-
+	insertOne: function(columns, values, callback) {
+		orm.insertOne("burgers", columns, values, function(res) {
+			callback(res);
+		});
 	},
-	updateOne: function() {
-
+	updateOne: function(objColVals, condition, callback) {
+		orm.updateOne("burgers", objColVals, condition, function(res) {
+			callback(res);
+		});
 	}
 };
 
